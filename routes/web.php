07e36fee0d0b3cf7f57f14929ae862c8d6e1d51a
pistissolutions <amodedun@gmail.com>
2019-06-11 11:35:08 +0100

    auth issue

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,4 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/{vue_capture?}', function () {
     return view('layouts.master');
-})->where('vue_capture', '[\/\w\.-]*');
+})->where('vue_capture', '[\/\w\.-]*')->middleware('auth');
